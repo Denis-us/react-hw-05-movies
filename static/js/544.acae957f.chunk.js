392e0ef2d10d9e4260b0ce314cb4c24d3f1f3b35
Denis-us <75166604+Denis-us@users.noreply.github.com>
2022-05-28 22:27:41 +0300

    Updates

---
+++ static/js/544.acae957f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_hw_05_movies=self.webpackChunkreact_hw_05_movies||[]).push([[544],{5544:function(e,s,n){n.r(s),n.d(s,{default:function(){return t}});var c=n(501),i="Navigation_link__TUbWc",a=n(184);function t(){return(0,a.jsxs)("nav",{children:[(0,a.jsx)(c.OL,{to:"/",className:i,children:"Home"}),(0,a.jsx)(c.OL,{to:"/movies",className:i,children:"Movies"})]})}}}]);
+//# sourceMappingURL=544.acae957f.chunk.js.map
